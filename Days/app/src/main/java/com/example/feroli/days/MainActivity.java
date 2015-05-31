a49a74bd10458bf15111c6478b4455b455fd8738
Fernando Ania <Feroli@Fernandos-MacBook-Pro.local>
2015-05-31 04:38:35 +0200

    added a chart

--- Days/app/src/main/java/com/example/feroli/days/MainActivity.java
+++ Days/app/src/main/java/com/example/feroli/days/MainActivity.java
@@ -1,35 +1,22 @@
 package com.example.feroli.days;
 
 import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
-import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.Legend.LegendPosition;
-import com.github.mikephil.charting.data.DataSet;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
-import com.github.mikephil.charting.utils.Highlight;
-import com.github.mikephil.charting.utils.PercentFormatter;
 
+import com.lylc.widget.circularprogressbar.CircularProgressBar;
 import com.melnykov.fab.FloatingActionButton;
 
-import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements OnChartValueSelectedListener{
+public class MainActivity extends AppCompatActivity{
 
     String addGoalInput;
     protected String[] mParties = new String[] {
@@ -37,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements OnChartValueSelec
     };
 
     private PieChart mChart;
+
+    private CircularProgressBar mCProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,14 +69,15 @@ public class MainActivity extends AppCompatActivity implements OnChartValueSelec
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Title");
 
-                final EditText input = new EditText(MainActivity.this);
-                input.setInputType(InputType.TYPE_CLASS_TEXT);
-                builder.setView(input);
+//                /final EditText input = new EditText(MainActivity.this);
+//                input.setInputType(InputType.TYPE_CLASS_TEXT);
+//                builder.setView(input);*/
 
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        addGoalInput = input.getText().toString();
+                        //addGoalInput = input.getText().toString();
+                        mCProgress.setProgress(mCProgress.getProgress()+5);
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -101,29 +91,14 @@ public class MainActivity extends AppCompatActivity implements OnChartValueSelec
             }
         });
 
-        mChart = (PieChart) findViewById(R.id.chart1);
-        mChart.setUsePercentValues(true);
-        mChart.setDragDecelerationFrictionCoef(0.95f);
-        mChart.setDrawHoleEnabled(true);
-        mChart.setHoleColorTransparent(true);
-        mChart.setTransparentCircleColor(Color.WHITE);
-        mChart.setHoleRadius(58f);
-        mChart.setTransparentCircleRadius(61f);
-        mChart.setDrawCenterText(true);
-        mChart.setRotationAngle(0);
-        mChart.setOnChartValueSelectedListener(this);
-        mChart.setCenterText("Estudiar");
-        mChart.animateY(1500, Easing.EasingOption.EaseInOutQuad);
+        mCProgress = (CircularProgressBar) findViewById(R.id.circularprogressbar1);
+        mCProgress.setProgress(5);
+        mCProgress.setTitle("Tarea");
+        mCProgress.setSubTitle("");
 
-        setData(1, 21);
-
-
-        Legend l = mChart.getLegend();
-        l.setPosition(LegendPosition.RIGHT_OF_CHART);
-        l.setXEntrySpace(7f);
-        l.setYEntrySpace(5f);
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -133,129 +108,7 @@ public class MainActivity extends AppCompatActivity implements OnChartValueSelec
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.actionToggleValues: {
-                for (DataSet<?> set : mChart.getData().getDataSets())
-                    set.setDrawValues(!set.isDrawValuesEnabled());
-
-                mChart.invalidate();
-                break;
-            }
-            case R.id.actionToggleHole: {
-                if (mChart.isDrawHoleEnabled())
-                    mChart.setDrawHoleEnabled(false);
-                else
-                    mChart.setDrawHoleEnabled(true);
-                mChart.invalidate();
-                break;
-            }
-            case R.id.actionDrawCenter: {
-                if (mChart.isDrawCenterTextEnabled())
-                    mChart.setDrawCenterText(false);
-                else
-                    mChart.setDrawCenterText(true);
-                mChart.invalidate();
-                break;
-            }
-            case R.id.actionToggleXVals: {
-
-                mChart.setDrawSliceText(!mChart.isDrawSliceTextEnabled());
-                mChart.invalidate();
-                break;
-            }
-            case R.id.actionSave: {
-                //mChart.saveToGallery("title"+System.currentTimeMillis());
-                mChart.saveToPath("title" + System.currentTimeMillis(), "");
-                break;
-            }
-            case R.id.actionTogglePercent:
-                mChart.setUsePercentValues(!mChart.isUsePercentValuesEnabled());
-                mChart.invalidate();
-                break;
-            case R.id.animateX: {
-                mChart.animateX(1800);
-                break;
-            }
-            case R.id.animateY: {
-                mChart.animateY(1800);
-                break;
-            }
-            case R.id.animateXY: {
-                mChart.animateXY(1800, 1800);
-                break;
-            }
-        }
         return true;
     }
-
-    private void setData(int count, float range) {
-
-        float mult = range;
-
-        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
-
-        // IMPORTANT: In a PieChart, no values (Entry) should have the same
-        // xIndex (even if from different DataSets), since no values can be
-        // drawn above each other.
-        for (int i = 0; i < count + 1; i++) {
-            yVals1.add(new Entry((float) (Math.random() * mult) + mult / 5, i));
-        }
-
-        ArrayList<String> xVals = new ArrayList<String>();
-
-        for (int i = 0; i < count + 1; i++)
-            xVals.add(mParties[i % mParties.length]);
-
-        PieDataSet dataSet = new PieDataSet(yVals1, "Calendario");
-        //dataSet.setSliceSpace(3f);
-        //dataSet.setSelectionShift(5f);
-
-        ArrayList<Integer> colors = new ArrayList<Integer>();
-
-        for (int c : ColorTemplate.VORDIPLOM_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.JOYFUL_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.COLORFUL_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.LIBERTY_COLORS)
-            colors.add(c);
-
-        for (int c : ColorTemplate.PASTEL_COLORS)
-            colors.add(c);
-
-        colors.add(ColorTemplate.getHoloBlue());
-
-        dataSet.setColors(colors);
-
-        PieData data = new PieData(xVals, dataSet);
-        data.setValueFormatter(new PercentFormatter());
-        data.setValueTextSize(11f);
-        data.setValueTextColor(Color.WHITE);
-        mChart.setData(data);
-
-        mChart.highlightValues(null);
-        mChart.invalidate();
-    }
-
-    @Override
-    public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
-
-        if (e == null)
-            return;
-        Log.i("VAL SELECTED",
-                "Value: " + e.getVal() + ", xIndex: " + e.getXIndex()
-                        + ", DataSet index: " + dataSetIndex);
-    }
-
-    @Override
-    public void onNothingSelected() {
-        Log.i("PieChart", "nothing selected");
-    }
-
 
 }
--- Days/app/src/main/java/com/example/feroli/days/SplashActivity.java
+++ Days/app/src/main/java/com/example/feroli/days/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.feroli.days;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 import java.util.Random;
 
 
-public class SplashActivity extends ActionBarActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +20,7 @@ public class SplashActivity extends ActionBarActivity {
         setContentView(R.layout.activity_splash);
         TextView tv = (TextView) findViewById(R.id.phrase);
 
-        String[] phrases = new String[] {"Frase 1", "Frase 2", "Frase 3", "Frase 4" };
+        String[] phrases = new String[] {"Develop one habit at a time.", "Keep your new habit small.", "Commit to your new habit for three weeks.", "Concentrate on starting.","Recognize the positive in what you’re doing.","Don’t miss two days in a row." };
 
         int index = new Random().nextInt(phrases.length);
 
